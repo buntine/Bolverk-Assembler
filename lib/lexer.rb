13d78dcce64632905a4405e521f40d5e76da0252
Andy Buntine <info@andrewbuntine.com>
2010-03-04 23:05:08 +1100

    Fixed ambiguity in tokens by just making hackers use decimal for numbers

--- lib/lexer.rb
+++ lib/lexer.rb
@@ -11,14 +11,15 @@ end
 class Bolverk::ASM::Lexer
 
   @@scan_table = [
-    [[/,/, 2],   [/-/, 6],   [/[a-zA-Z]/, 3],   [/[0-9]/, 4],   [/\n/, 9],   [/\s|\t/, 9],   [/.*/, nil]],
+    [[/,/, 2],   [/-/, 7],   [/[a-zA-Z]/, 3],   [/[0-9]/, 4],   [/\n/, 9],   [/\s|\t/, 9],   [/.*/, nil]],
     [[/,/, nil], [/-/, nil], [/[a-zA-Z]/, nil], [/[0-9]/, nil], [/\n/, nil], [/\s|\t/, nil], [/.*/, nil]],
     [[/,/, nil], [/-/, nil], [/[a-zA-Z]/, 3],   [/[0-9]/, nil], [/\n/, nil], [/\s|\t/, nil], [/.*/, nil]],
     [[/,/, nil], [/-/, nil], [/[a-zA-Z]/, nil], [/[0-9]/, 5],   [/\n/, nil], [/\s|\t/, nil], [/.*/, nil]],
+    [[/,/, nil], [/-/, nil], [/[a-zA-Z]/, nil], [/[0-9]/, 6],   [/\n/, nil], [/\s|\t/, nil], [/.*/, nil]],
     [[/,/, nil], [/-/, nil], [/[a-zA-Z]/, nil], [/[0-9]/, nil], [/\n/, nil], [/\s|\t/, nil], [/.*/, nil]],
-    [[/,/, nil], [/-/, 7],   [/[a-zA-Z]/, nil], [/[0-9]/, nil], [/\n/, nil], [/\s|\t/, nil], [/.*/, nil]],
-    [[/,/, 7],   [/-/, 7],   [/[a-zA-Z]/, 7],   [/[0-9]/, 7],   [/\n/, 9],   [/\s|\t/, 7],   [/.*/, 7]],
-    [[/,/, nil], [/-/, nil], [/[a-zA-Z]/, nil], [/[0-9]/, nil], [/\n/, 8],   [/\s|\t/, 8],   [/.*/, nil]],
+    [[/,/, nil], [/-/, 8],   [/[a-zA-Z]/, nil], [/[0-9]/, nil], [/\n/, nil], [/\s|\t/, nil], [/.*/, nil]],
+    [[/,/, 8],   [/-/, 8],   [/[a-zA-Z]/, 8],   [/[0-9]/, 8],   [/\n/, 10],   [/\s|\t/, 8],  [/.*/, 8]],
+    [[/,/, nil], [/-/, nil], [/[a-zA-Z]/, nil], [/[0-9]/, nil], [/\n/, 9],   [/\s|\t/, 9],   [/.*/, nil]],
     [[/,/, nil], [/-/, nil], [/[a-zA-Z]/, nil], [/[0-9]/, nil], [/\n/, nil], [/\s|\t/, nil], [/.*/, nil]]
   ]
 
@@ -28,6 +29,7 @@ class Bolverk::ASM::Lexer
     :keyword,
     :number,
     :number,
+    :number,
     nil,
     nil,
     :whitespace,
@@ -35,9 +37,10 @@ class Bolverk::ASM::Lexer
   ]
 
   def initialize(stream)
+    attr_reader :tokens
+
     @stream = stream
     @tokens = []
-    @state = 1
   end
 
   # Scans the program input and returns an array of tokens in the
@@ -88,7 +91,7 @@ class Bolverk::ASM::Lexer
             break
           # Fatal lexical error.
           else
-            raise Bolverk::ASM::LexicalError "Disallowed token: #{char}"
+            raise Bolverk::ASM::LexicalError, "Disallowed token: #{char}"
           end
         end
       end
@@ -111,7 +114,7 @@ class Bolverk::ASM::Lexer
   end
 
   def is_whitespace_or_comment?
-    [8, 9].include?(@state)
+    [9, 10].include?(@state)
   end
 
 end
