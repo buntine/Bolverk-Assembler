eb320495c18298bd104b1305ea2975fec53b5005
Andy Buntine <info@andrewbuntine.com>
2010-03-04 22:27:30 +1100

    Simple, unfactored lexer is done. Needs work to fix ambiguity between hex numbers and identifiers

--- lib/lexer.rb
+++ lib/lexer.rb
@@ -11,15 +11,15 @@ end
 class Bolverk::ASM::Lexer
 
   @@scan_table = [
-    [[/,/, 2],   [/-/, 6],   [/a-zA-Z/, 3],   [/0-9/, 4],   [/\n/, 9],   [/\s|\t/, 9]],
-    [[/,/, nil], [/-/, nil], [/a-zA-Z/, nil], [/0-9/, nil], [/\n/, nil], [/\s|\t/, nil]],
-    [[/,/, nil], [/-/, nil], [/a-zA-Z/, 3],   [/0-9/, nil], [/\n/, nil], [/\s|\t/, nil]],
-    [[/,/, nil], [/-/, nil], [/a-zA-Z/, nil], [/0-9/, 5],   [/\n/, nil], [/\s|\t/, nil]],
-    [[/,/, nil], [/-/, nil], [/a-zA-Z/, nil], [/0-9/, nil], [/\n/, nil], [/\s|\t/, nil]],
-    [[/,/, nil], [/-/, 7],   [/a-zA-Z/, nil], [/0-9/, nil], [/\n/, nil], [/\s|\t/, nil]],
-    [[/,/, 7],   [/-/, 7],   [/a-zA-Z/, 7],   [/0-9/, 7],   [/\n/, 9],   [/\s|\t/, 7]],
-    [[/,/, nil], [/-/, nil], [/a-zA-Z/, nil], [/0-9/, nil], [/\n/, 8],   [/\s|\t/, 8]],
-    [[/,/, nil], [/-/, nil], [/a-zA-Z/, nil], [/0-9/, nil], [/\n/, nil], [/\s|\t/, nil]]
+    [[/,/, 2],   [/-/, 6],   [/[a-zA-Z]/, 3],   [/[0-9]/, 4],   [/\n/, 9],   [/\s|\t/, 9],   [/.*/, nil]],
+    [[/,/, nil], [/-/, nil], [/[a-zA-Z]/, nil], [/[0-9]/, nil], [/\n/, nil], [/\s|\t/, nil], [/.*/, nil]],
+    [[/,/, nil], [/-/, nil], [/[a-zA-Z]/, 3],   [/[0-9]/, nil], [/\n/, nil], [/\s|\t/, nil], [/.*/, nil]],
+    [[/,/, nil], [/-/, nil], [/[a-zA-Z]/, nil], [/[0-9]/, 5],   [/\n/, nil], [/\s|\t/, nil], [/.*/, nil]],
+    [[/,/, nil], [/-/, nil], [/[a-zA-Z]/, nil], [/[0-9]/, nil], [/\n/, nil], [/\s|\t/, nil], [/.*/, nil]],
+    [[/,/, nil], [/-/, 7],   [/[a-zA-Z]/, nil], [/[0-9]/, nil], [/\n/, nil], [/\s|\t/, nil], [/.*/, nil]],
+    [[/,/, 7],   [/-/, 7],   [/[a-zA-Z]/, 7],   [/[0-9]/, 7],   [/\n/, 9],   [/\s|\t/, 7],   [/.*/, 7]],
+    [[/,/, nil], [/-/, nil], [/[a-zA-Z]/, nil], [/[0-9]/, nil], [/\n/, 8],   [/\s|\t/, 8],   [/.*/, nil]],
+    [[/,/, nil], [/-/, nil], [/[a-zA-Z]/, nil], [/[0-9]/, nil], [/\n/, nil], [/\s|\t/, nil], [/.*/, nil]]
   ]
 
   @@token_list = [
@@ -44,19 +44,26 @@ class Bolverk::ASM::Lexer
   # following format:
   #   { "type" => :keyword, "value" => "STOR", "line" => 3 }
   def scan
+    while !@stream.eof?
+      @tokens << next_token
+    end
+
+    @tokens
   end
 
  private
 
   # Attempts to find the next token in the stream.
-  def next_token()
-    char = @stream.read(1)
-    value = []
-    next_state = get_next_state(char)
+  def next_token
+    @state = 1
+    value = ""
     remembered_state = 0
     remembered_chars = []
 
-    while true
+    while !@stream.eof?
+      char = @stream.read(1)
+      next_state = get_next_state(char)
+
       # Move to the next state.
       if next_state
         if recognizable?
@@ -67,7 +74,6 @@ class Bolverk::ASM::Lexer
         value << char
         remembered_chars << char
         @state = next_state
-        next
       else
         # Recognise the final token.
         if recognizable?
@@ -78,7 +84,7 @@ class Bolverk::ASM::Lexer
           if remembered_state > 0
             @state = remembered_state
             @stream.seek(@stream.pos - remembered_chars.length)
-            value = value.filter { |c| !remembered_chars.include?(c) }
+            value = value[0..-(remembered_chars.length + 1)]
             break
           # Fatal lexical error.
           else
@@ -89,20 +95,19 @@ class Bolverk::ASM::Lexer
     end
 
     if is_whitespace_or_comment?
-      @state = 1
       next_token
     else
-      { "type" => @@token_list[@state], "value" => value.join("") }
+      { "type" => @@token_list[@state - 1], "value" => value }
     end
   end
 
   def recognizable?
-    !!@@token_list[@state]
+    !!@@token_list[@state - 1]
   end
 
   # Attempts to match the character against one of the scan rules.
   def get_next_state(char)
-    @@scan_table[@state].find(lambda {[0, 1]}) { |rule| char =~ rule[0] }[1]
+    @@scan_table[@state - 1].find(lambda {[0, 1]}) { |rule| char =~ rule[0] }[1]
   end
 
   def is_whitespace_or_comment?
