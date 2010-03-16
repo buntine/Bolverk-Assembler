aeebeb6b6a6037bdfbdf74d760d75e75680d2e0a
Andrew Buntine <perunwit@perunwit-devbox.(none)>
2010-03-16 21:30:05 +1100

    Started fleshing out Generator

--- lib/asm.rb
+++ lib/asm.rb
@@ -5,6 +5,7 @@ module Bolverk::ASM; end
 # Setup custom exceptions.
 class Bolverk::ASM::LexicalError < Exception; end
 class Bolverk::ASM::SyntaxError < Exception; end
+class Bolverk::ASM::SemanticError < Exception; end
 
 # Begin the loading sequence.
 require File.dirname(__FILE__) + "/monkeypatches/array"
--- lib/asm/generator.rb
+++ lib/asm/generator.rb
@@ -3,11 +3,11 @@ class Bolverk::ASM::Generator
   # A code-generator. This constitutes the back-end of the compiler.
   # I'm taking a bit of an ad-hoc approach here, but basically the
   # generator accepts a Parse Tree and simplifies it into a Syntax
-  # Tree. The Syntax Tree is then parsed and code is generated on
-  # the fly.
+  # Tree. The members of the Syntax Tree are then evaluated to
+  # produce the final program source.
   #
   # Some semantic analysis (procedure exists? correct number of
-  # arguments?) takes place during the first pass.
+  # arguments?) takes place during the evaluation pass.
 
   def initialize(parse_tree)
     @parse_tree = parse_tree
@@ -16,6 +16,42 @@ class Bolverk::ASM::Generator
   # Analyses the parse tree and produces the final source for the
   # target machine.
   def generate
+    tree = generate_syntax_tree
+    source = []
+  
+    tree[0..-1].each do |statement|
+      procedure = statement[1]
+
+      # Check some semantics.
+      assert_proc_exists(procedure)
+      assert_correct_args(procedure, statement[2..-1])
+
+      source << Bolverk::ASM::Procedures.send(procedure, statement[2..-1])
+    end
+
+    source.join("\n")
+  end
+
+ private
+
+  # Simplifies the parse tree into an abstract syntax tree
+  # for evaluation.
+  def generate_syntax_tree
+    [:program,
+      [:statement,
+        "KEYWORD",
+        "NUMBER",
+        "NUMBER"],
+      [:statement,
+        "KEYWORD",
+        "NUMBER"]]
+  end
+
+  def assert_proc_exists(name)
+
+  end
+
+  def assert_correct_args(name, args)
 
   end
 
