61dbe797e58cff1c245afb6fe519774efc0bdfe6
Andrew Buntine <perunwit@perunwit-devbox.(none)>
2010-03-18 01:53:33 +1100

    Added in algorithm to reduce parse tree into AST

--- lib/asm/generator.rb
+++ lib/asm/generator.rb
@@ -23,14 +23,14 @@ class Bolverk::ASM::Generator
     tree = generate_syntax_tree
     source = []
   
-    tree[0..-1].each do |statement|
-      procedure = statement[1]
+    tree.butfirst.each do |statement|
+      procedure = statement.first
 
       # Check some semantics.
       assert_proc_exists(procedure)
-      assert_correct_args(procedure, statement[2..-1])
+      assert_correct_args(procedure, statement.butfirst)
 
-      source << self.send("proc_#{procedure}", *statement[2..-1])
+      source << self.send("proc_#{procedure}", *statement.butfirst)
     end
 
     source.join("\n")
@@ -41,14 +41,45 @@ class Bolverk::ASM::Generator
   # Simplifies the parse tree into an abstract syntax tree
   # for evaluation.
   def generate_syntax_tree
-    [:program,
-      [:statement,
-        "KEYWORD",
-        "NUMBER",
-        "NUMBER"],
-      [:statement,
-        "KEYWORD",
-        "NUMBER"]]
+    build_ast(@parse_tree)[0]
+  end
+
+  # Actually builds the AST from the parse tree. This method
+  # is mutually recursive with build_ast_in_forest.
+  def build_ast(tree)
+    if leaf?(tree)
+      is_useful_terminal?(tree.first) ? tree : []
+    else
+      if is_useful_non_terminal?(tree.first)
+        [[tree.first] + syntax_tree_in_forest(tree.butfirst)]
+      else
+        syntax_tree_in_forest(tree.butfirst)
+      end
+    end
+  end
+
+  # Helper method for build_ast.
+  def build_ast_in_forest(trees)
+    if trees.empty?
+      []
+    else
+      syntax_tree(trees.first) + syntax_tree_in_forest(trees.butfirst)
+    end
+  end
+
+  # These are the only non-terminals we really need to keep in
+  # the AST.
+  def is_useful_non_terminal?(symbol)
+    [ :program, :statement ].include?(symbol)
+  end
+
+  # We will keep all terminals except for the EOF pseudo-token.
+  def is_useful_terminal?(symbol)
+    symbol.is_a?(Hash) and symbol[:type] != :eof
+  end
+
+  def leaf?(tree)
+    tree.length == 1
   end
 
 end
--- lib/monkeypatches/array.rb
+++ lib/monkeypatches/array.rb
@@ -12,4 +12,14 @@ class Array
       self[0..-2]
     end
   end
+
+  # Returns everything except the first element (Lisp-style cdr).
+  def butfirst
+    if self.empty?
+      []
+    else
+      self[1..-1]
+    end
+  end
+
 end
