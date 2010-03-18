979635272600351e1e5fd10efb818341278ed794
Andrew Buntine <perunwit@perunwit-devbox.(none)>
2010-03-19 10:26:27 +1100

    Added more tests for generator

--- test/functional/generator.rb
+++ test/functional/generator.rb
@@ -9,12 +9,12 @@ class GeneratorTest < Test::Unit::TestCase
     path = lambda { |file| File.join(File.dirname(__FILE__), "data", file) }
 
     @tokens_a = Bolverk::ASM::Lexer.new(File.open(path.call("valid_a.basm"))).scan
-    @tokens_b = Bolverk::ASM::Lexer.new(File.open(path.call("valid_b.basm"))).scan
     @tokens_c = Bolverk::ASM::Lexer.new(File.open(path.call("valid_c.basm"))).scan
+    @tokens_d = Bolverk::ASM::Lexer.new(File.open(path.call("valid_d.basm"))).scan
 
     @program_a = Bolverk::ASM::Parser.new(@tokens_a)
-    @program_b = Bolverk::ASM::Parser.new(@tokens_b)
     @program_c = Bolverk::ASM::Parser.new(@tokens_c)
+    @program_d = Bolverk::ASM::Parser.new(@tokens_d)
   end
 
   def test_program_a_should_generate_valid_machine_code
@@ -22,6 +22,13 @@ class GeneratorTest < Test::Unit::TestCase
     @generator_a = Bolverk::ASM::Generator.new(@tree_a)
 
     assert_equal(@generator_a.generate, "2143\n227c\n5123\n3363\nd163")
+  end
+
+  def test_program_d_should_generate_valid_machine_code
+    @tree_d = @program_d.parse
+    @generator_d = Bolverk::ASM::Generator.new(@tree_d)
+
+    assert_equal(@generator_d.generate, "2548\n2669\n22fe\n5123\n3363\nd163")
   end
 
   # One of the calls to LOAD only supplies one argument.
