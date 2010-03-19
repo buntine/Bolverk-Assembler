9faa002f498b9bb4c74b5333440a3c02b1cfa2c8
Andrew Buntine <perunwit@perunwit-devbox.(none)>
2010-03-19 23:07:11 +1100

    Added all remaining native instruction evaluators

--- lib/asm/procedures.rb
+++ lib/asm/procedures.rb
@@ -5,20 +5,76 @@ module Bolverk::ASM::Procedures
   # Each method starting with "proc_" should generate and return
   # the correct machine code for it's operation.
 
+  def proc_meml(register, memory_cell)
+    "1" + h(register) + h(memory_cell)
+  end
+
+  def proc_vall(register, value)
+    "2" + h(register) + h(value)
+  end
+
   def proc_stor(register, memory_cell)
     "3" + h(register) + h(memory_cell)
   end
 
-  def proc_load(register, value)
-    "2" + h(register) + h(value)
+  def proc_move(a, b)
+    "40" + h(a) + h(b)
   end
 
   def proc_badd(a, b, result)
     "5" + h(a) + h(b) + h(result)
   end
 
+  def proc_fadd(a, b, result)
+    "6" + h(a) + h(b) + h(result)
+  end
+
+  def proc_or(a, b, result)
+    "7" + h(a) + h(b) + h(result)
+  end
+
+  def proc_or(a, b, result)
+    "7" + h(a) + h(b) + h(result)
+  end
+
+  def proc_and(a, b, result)
+    "8" + h(a) + h(b) + h(result)
+  end
+
+  def proc_xor(a, b, result)
+    "9" + h(a) + h(b) + h(result)
+  end
+
+  def proc_rot(register, times)
+    "a" + h(register) + "0" + h(times)
+  end
+
+  def proc_jump(register, memory_cell)
+    "b" + h(register) + h(memory_cell)
+  end
+
+  def proc_pmch(memory_cell)
+    "d0" + h(memory_cell)
+  end
+
   def proc_pmde(memory_cell)
     "d1" + h(memory_cell)
+  end
+
+  def proc_pmfp(memory_cell)
+    "d2" + h(memory_cell)
+  end
+
+  def proc_pvch(memory_cell)
+    "e0" + h(memory_cell)
+  end
+
+  def proc_pvde(memory_cell)
+    "e1" + h(memory_cell)
+  end
+
+  def proc_pvfp(memory_cell)
+    "e2" + h(memory_cell)
   end
 
   # Semantic check to ensure given procedure exists.
