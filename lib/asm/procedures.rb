56fb536df086d55a93e8a5e775a693c5f6af1001
Andrew Buntine <perunwit@perunwit-devbox.(none)>
2010-03-20 12:48:40 +1100

    Changed the way the Procedures module works so metadata date be closely tied to each evaluator

--- lib/asm/procedures.rb
+++ lib/asm/procedures.rb
@@ -2,104 +2,105 @@ module Bolverk::ASM::Procedures
 
   # This module provides a substitution method for each procedure
   # defined in Bolverk assembly.
-  # Each method starting with "proc_" should generate and return
-  # the correct machine code for it's operation.
+  # Each member of the hash in the procedures method should
+  # define the method of evaluation and additional metadata such as
+  # the number of arguments required, etc.
 
-  def proc_meml(register, memory_cell)
-    "1" + h(register) + h(memory_cell)
-  end
-
-  def proc_vall(register, value)
-    "2" + h(register) + h(value)
-  end
-
-  def proc_stor(register, memory_cell)
-    "3" + h(register) + h(memory_cell)
-  end
-
-  def proc_move(a, b)
-    "40" + h(a) + h(b)
-  end
-
-  def proc_badd(a, b, result)
-    "5" + h(a) + h(b) + h(result)
-  end
-
-  def proc_fadd(a, b, result)
-    "6" + h(a) + h(b) + h(result)
-  end
-
-  def proc_or(a, b, result)
-    "7" + h(a) + h(b) + h(result)
-  end
-
-  def proc_and(a, b, result)
-    "8" + h(a) + h(b) + h(result)
-  end
-
-  def proc_xor(a, b, result)
-    "9" + h(a) + h(b) + h(result)
-  end
-
-  def proc_rot(register, times)
-    "a" + h(register) + "0" + h(times)
-  end
-
-  def proc_jump(register, memory_cell)
-    "b" + h(register) + h(memory_cell)
-  end
-
-  def proc_pmch(memory_cell)
-    "d0" + h(memory_cell)
-  end
-
-  def proc_pmde(memory_cell)
-    "d1" + h(memory_cell)
-  end
-
-  def proc_pmfp(memory_cell)
-    "d2" + h(memory_cell)
-  end
-
-  def proc_pvch(memory_cell)
-    "e0" + h(memory_cell)
-  end
-
-  def proc_pvde(memory_cell)
-    "e1" + h(memory_cell)
-  end
-
-  def proc_pvfp(memory_cell)
-    "e2" + h(memory_cell)
-  end
-
-  def proc_writ(value, memory_cell)
-    [ proc_vall("F", value),
-      proc_stor("F", memory_cell) ].join("\n")
+  def procedures
+    { "meml" => { :args => 2,
+                  :method => lambda do |r, mc|
+                               "1" + h(r) + h(mc)
+                             end },
+      "vall" => { :args => 2,
+                  :method => lambda do |r, mc|
+                               "2" + h(r) + h(mc)
+                             end },
+      "stor" => { :args => 2,
+                  :method => lambda do |r, mc|
+                               "3" + h(r) + h(mc)
+                             end },
+      "move" => { :args => 2,
+                  :method => lambda do |ra, rb|
+                               "40" + h(ra) + h(rb)
+                             end },
+      "badd" => { :args => 3,
+                  :method => lambda do |ra, rb, rc|
+                               "5" + h(ra) + h(rb) + h(rc)
+                             end },
+      "fadd" => { :args => 3,
+                  :method => lambda do |ra, rb, rc|
+                               "6" + h(ra) + h(rb) + h(rc)
+                             end },
+      "or"   => { :args => 3,
+                  :method => lambda do |ra, rb, rc|
+                               "7" + h(ra) + h(rb) + h(rc)
+                             end },
+      "and"  => { :args => 3,
+                  :method => lambda do |ra, rb, rc|
+                               "8" + h(ra) + h(rb) + h(rc)
+                             end },
+      "xor"  => { :args => 3,
+                  :method => lambda do |ra, rb, rc|
+                               "9" + h(ra) + h(rb) + h(rc)
+                             end },
+      "rot"  => { :args => 2,
+                  :method => lambda do |r, n|
+                               "a" + h(r) + h(n)
+                             end },
+      "jump" => { :args => 2,
+                  :method => lambda do |r, mc|
+                               "b" + h(r) + h(mc)
+                             end },
+      "halt" => { :args => 0,
+                  :method => lambda do
+                               "c000"
+                             end },
+      "pmch" => { :args => 1,
+                  :method => lambda do |mc|
+                               "d0" + h(mc)
+                             end },
+      "pmde" => { :args => 1,
+                  :method => lambda do |mc|
+                               "d1" + h(mc)
+                             end },
+      "pmfp" => { :args => 1,
+                  :method => lambda do |mc|
+                               "d2" + h(mc)
+                             end },
+      "pvch" => { :args => 1,
+                  :method => lambda do |mc|
+                               "e0" + h(mc)
+                             end },
+      "pvde" => { :args => 1,
+                  :method => lambda do |mc|
+                               "e1" + h(mc)
+                             end },
+      "pvfp" => { :args => 1,
+                  :method => lambda do |mc|
+                               "e2" + h(mc)
+                             end },
+      "writ" => { :args => 2,
+                  :method => lambda do |v, mc|
+                               [ "2f" + h(v),
+                                 "3f" + h(mc) ].join("\n")
+                             end } }
   end
 
   # Semantic check to ensure given procedure exists.
   def assert_proc_exists(token)
     name = p_name(token)
-    unless self.respond_to?("proc_#{name}")
+    unless procedures.include?(name)
       raise Bolverk::ASM::SemanticError, "Unknown procedure: #{name}"
     end
   end
 
   # Sementic check to ensure the given procedure
   # will accept the given arguments.
-  # TODO: Work out better way to encode the argument
-  #       count for each procedure.
   def assert_correct_args(token, args)
     name = p_name(token)
-    arg_count = { "meml" => 2, "vall" => 2, "stor" => 2, "move" => 2,
-                  "badd" => 3, "fadd" => 3, "or" => 3, "and" => 3,
-                  "xor" => 3, "rot" => 2, "jump" => 2, "pmch" => 1,
-                  "pmde" => 1, "pmfp" => 1, "pvch" => 1, "pvde" => 1,
-                  "pvfp" => 1 }
 
-    unless arg_count[name] == args.length
-      raise Bolverk::ASM::SemanticError, "Procedure #{name} requires #{arg_count[name]} arguments. Given #{args.length}."
+    unless procedures[name][:args] == args.length
+      raise Bolverk::ASM::SemanticError, "Procedure #{name} requires #{procedures[name][:args]} arguments. Given #{args.length}."
     end
   end
 
@@ -112,7 +113,7 @@ module Bolverk::ASM::Procedures
       (v =~ /^\d+$/) ? v.to_i : v[0]
     end
 
-    self.send("proc_#{p_name(token)}", *arguments)
+    procedures[p_name(token)][:method].call(*arguments)
   end
 
  private
